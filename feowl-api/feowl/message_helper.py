a77c58e228e68033de9b230456f36cf91830e3dc
Tim Kleinschmidt <tim@tesobe.com>
2012-08-17 11:31:24 +0200

    First Version of contribute
    Supports
    - Check if device is existing
    - Check if contributor already registered else register a new one
    - Check if we ask this contributor to contribute

--- feowl-api/feowl/message_helper.py
+++ feowl-api/feowl/message_helper.py
@@ -39,6 +39,34 @@ def send_message(users, message, channel):
     connection.close()
 
 
+def contribute(message_array, mobile_number):
+    """
+        Message: contribute <duration>, <area>
+    """
+    try:
+        device = Device.objects.get(phone_number=mobile_number)
+        # Check if user exist else create a inactive user
+        if device.contributor == None:
+            try:
+                contributor = Contributor(name=mobile_number, email=mobile_number + "@feowl.com", status=Contributor.INACTIVE)
+                contributor.save()
+                device.contributor = contributor.id
+                device.save()
+            except IntegrityError, e:
+                msg = e.message
+                if msg.find("name") != -1:
+                    return "Name already exist. Please use an other one"
+                elif msg.find("email") != -1:
+                    return "Email already exist. Please use an other one."
+                return "Unkown Error please try later to register"
+            return "User is created"  # END
+        # Check if we ask this user to contribute
+        if device.contributor.enquiry != datetime.today().date():  # Maybe we have to check if it was yesterday
+            return "We dont ask this user"  # END
+    except DoesNotExist:
+        raise
+
+
 def register(message_array, mobile_number):
     """
         Message: register <contributor_name>
@@ -92,7 +120,7 @@ def read_message(message):
     mobile_number = "test_number"
     index, keyword, message_array = parse(message)
     if keyword == "contribute":
-        pass
+        contribute(message_array, mobile_number)
     elif keyword == "help":
         pass
     elif keyword == "register":

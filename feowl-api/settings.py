414acb52348dff9a8eb66251eb4e299500b78009
Ismail <ismail@tesobe.com>
2012-10-22 14:10:24 +0200

    sending welcome email at registration implemented

--- feowl-api/settings.py
+++ feowl-api/settings.py
@@ -233,3 +233,9 @@ ANONYMOUS_USER_NAME = "Anonymous"
 ANONYMOUS_EMAIL = "anonymous@feowl.com"
 NEWSLETTER_FROM = "newsletter@feowl.com"
 REGISTRATION_FROM = "registration@feowl.com"
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'i_chaib@esi.dz'
+EMAIL_HOST_PASSWORD = 'clart&obscure16'

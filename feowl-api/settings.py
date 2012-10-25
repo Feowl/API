de671c48293c07df4ae3e2443c0bd073f2f6ede2
Ismail <ismail@tesobe.com>
2012-10-25 14:31:40 +0200

    Email settings updated with the new iamfeowl email account

--- feowl-api/settings.py
+++ feowl-api/settings.py
@@ -235,3 +235,8 @@ NEWSLETTER_FROM = "newsletter@feowl.com"
 REGISTRATION_FROM = "registration@feowl.com"
 
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'iamfeowl@gmail.com'
+EMAIL_HOST_PASSWORD = 'nBTKquaKqTE'

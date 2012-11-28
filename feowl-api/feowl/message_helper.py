882f67a073308d22b8ab085c5d080a6aedfb9108
Ismail <ismail@tesobe.com>
2012-11-28 12:05:42 +0100

    making Area lookup case insensitive

--- feowl-api/feowl/message_helper.py
+++ feowl-api/feowl/message_helper.py
@@ -179,7 +179,7 @@ def get_all_areas_name():
     areas = Area.objects.all()
     list = []
     for a in areas:
-        list.append(a.name)
+        list.append(a.name.lower())
     return list
 
 
@@ -187,9 +187,9 @@ def get_area(area_name):
     from difflib import get_close_matches
     from django.contrib.gis.geos import Polygon
     areas = get_all_areas_name()
-    corrected_area_name = get_close_matches(area_name, areas, 1)
+    corrected_area_name = get_close_matches(area_name.lower(), areas, 1)
     try:
-        area = Area.objects.get(name=corrected_area_name)
+        area = Area.objects.get(name__iexact=corrected_area_name)
     except Area.DoesNotExist:
         poly = Polygon(((0, 0), (0, 0), (0, 0), (0, 0), (0, 0)), ((0, 0), (0, 0), (0, 0), (0, 0), (0, 0)))
         area = Area.objects.create(name=area_name, overall_population=0, pop_per_sq_km=0, city="Douala", country="Cameroon", geometry=poly)
